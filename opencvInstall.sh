a0c6da82d849c8b0535da2688db0b0607253eb2d
ahmet <77435649+koskerahmet@users.noreply.github.com>
2021-03-17 10:32:08 +0300

    Add files via upload

---
+++ opencvInstall.sh
@@ -0,0 +1,31 @@
+#opencv install ubuntu
+
+#!/bin/sh
+
+echo "---------------------------------------"
+echo "pip3 is installing..."
+echo "---------------------------------------"
+
+sudo apt install python3-pip
+
+echo "---------------------------------------"
+echo "---------installed pip3 your pc--------"
+echo "---------------------------------------"
+
+echo "---------------------------------------"
+echo "pip3 is checking..."
+echo "---------------------------------------"
+
+pip3 --version
+
+echo "---------------------------------------"
+echo "opencv is installing for python language"
+echo "---------------------------------------"
+
+pip3 install opencv-python
+
+echo "---------------------------------------"
+echo "opencv is installed for python language"
+echo "---------------------------------------"
+
+
